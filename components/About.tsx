766075c5b8a08550b22c66de28e8c30f9181c867
Dagmar Drbálková <Daasa.D@seznam.cz>
2025-07-21 22:08:48 +0200

    feat: textation

--- components/About.tsx
+++ components/About.tsx
@@ -24,7 +24,7 @@ export default function About() {
               </p>
               
               <p data-macaly="about-paragraph-2">
-                K IT mě přivedli bratři - oba odjakživa programátoři, a&nbsp;já se rozhodla k&nbsp;nim přidat. Začala jsem intenzivním IT kurzem z&nbsp;programu IT Network s.r.o. a pokračovala samostudiem za pomocí Coursera a knih z&nbsp;O'Reilly, ale hlavně přes řadu větších i&nbsp;menších projektů.
+                K IT mě přivedli bratři - oba odjakživa programátoři, a&nbsp;já se rozhodla k&nbsp;nim přidat. Začala jsem intenzivním IT kurzem z&nbsp;programu IT Network s.r.o. a pokračovala samostudiem za pomocí Coursera a knih z&nbsp;O'Reilly, ale hlavně skrze řadu větších i&nbsp;menších projektů.
               </p>              
             </div>
 
